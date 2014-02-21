03ac12c2b221dddee24cd703dfcfe80ca0eec333
Sean Carolan <scarolan@opscode.com>
2014-02-21 16:04:20 -0600

    Initial commit.

---
+++ gen_message.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;34m'
+normal='\033[0m'
+bg_white='\033[1;47m'
+
+
+echo  "$green              _______________          $normal"
+echo  "$green             / ___/ __/_  __/          $normal"
+echo  "$green            / (_ / _/  / /             $normal"
+echo  "$green            \___/___/ /_/              $normal"
+echo  "$green          _______  ___________         $normal"
+echo  "$green         / __/ _ \/ __/ __/ _ \        $normal"
+echo  "$green        _\ \/ ___/ _// _// // /        $normal"
+echo  "$green       /___/_/  /___/___/____/         $normal"
+echo  "$blue             ___ ___ _____             $normal"
+echo  "$blue            / __| __|_   _|            $normal"
+echo  "$blue           | (_ | _|  | |              $normal"
+echo  "$blue            \___|___| |_|              $normal"
+echo  "$blue   ___      _____ ___  ___  __  __ ___ $normal"
+echo  "$blue  /_\ \    / / __/ __|/ _ \|  \/  | __|$normal"
+echo  "$blue / _ \ \/\/ /| _|\__ \ (_) | |\/| | _| $normal"
+echo  "$blue/_/ \_\_/\_/ |___|___/\___/|_|  |_|___|$normal"
+echo  "$red            _____ ____ ______          $normal"
+echo  "$red           / ___// __//_  __/          $normal"
+echo  "$red          / (_ // _/   / /             $normal"
+echo  "$red          \___//___/  /_/              $normal"
+echo  "$red        _____ __ __ ____ ____ __       $normal"
+echo  "$red       / ___// // // __// __// /       $normal"
+echo  "$red      / /__ / _  // _/ / _/ /_/        $normal"
+echo  "$red      \___//_//_//___//_/  (_)         $normal"
